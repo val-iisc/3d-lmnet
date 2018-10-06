70d51bbfde3e872b9ba7aede56ebe7c49ccf2960
priyanka <priyanka.mp2808@gmail.com>
2018-10-06 20:12:16 +0530

    Added makefile

--- scripts/metrics_lm.sh
+++ scripts/metrics_lm.sh
@@ -11,6 +11,7 @@ for cat in "${categs[@]}"; do
 	python metrics.py --gpu $gpu --data_dir ${data_dir} --exp $exp --mode lm --category $cat --load_best --bottleneck 512 --bn_decoder --eval_set ${eval_set} --batch_size 24
 done
 
+declare -a categs=("airplane" "bench" "cabinet" "car" "chair" "lamp" "monitor" "rifle" "sofa" "speaker" "table" "telephone" "vessel")
 for cat in "${categs[@]}"; do
 	echo ${cat}
 	cat ${exp}/metrics/${eval_set}/${cat}.csv
